560763ee986a87c4bbf9e9aa2fcee627cb7a15f5
Samuel Penn <sam@notasnark.net>
2026-04-12 12:13:00 +0100

    Better handling of damage on characteristics. Added a new effect type which modifies a characteristic for purposes of how much damage it can take. Cleaned up code to ensure the old chaAug effect doesn't modify the amount of damage.

--- mgt2e/module/helpers/config.mjs
+++ mgt2e/module/helpers/config.mjs
@@ -840,6 +840,7 @@ MGT2.getDefaultSkills = function() {
 
 MGT2.EFFECT_TYPES = {
   "CHA_AUG": "chaAug",
+  "CHA_BASE": "chaBase",
   "CHA_DM": "chaDM",
   "CHA_BOON": "chaBoon",
   "CHA_BANE": "chaBane",
@@ -851,6 +852,7 @@ MGT2.EFFECT_TYPES = {
 
 MGT2.EFFECTS = {
   "chaAug": { "targets": "char", "value": true, "property": "augment", mode: CONST.ACTIVE_EFFECT_MODES.ADD },
+  "chaBase": { "targets": "char", "value": true, "property": "value", mode: CONST.ACTIVE_EFFECT_MODES.ADD },
   "chaDM": { "targets": "char", "value": true, "property": "augdm", mode: CONST.ACTIVE_EFFECT_MODES.ADD },
   "chaBoon": { "targets": "char", "value": false, mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE },
   "chaBane":  { "targets": "char", "value": false, mode: CONST.ACTIVE_EFFECT_MODES.OVERRIDE },
